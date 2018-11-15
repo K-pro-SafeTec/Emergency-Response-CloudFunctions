aba72dbf1459c7712bd840543ac9d8fd82f690a1
Snorre Børtnes <snorrebortnes@gmail.com>
2018-11-15 13:33:27 +0100

    Deployment test
    Deployment test

--- HttpTriggerSinglePersonData/index.js
+++ HttpTriggerSinglePersonData/index.js
@@ -1,7 +1,7 @@
     var Connection = require('tedious').Connection;
     var Request = require('tedious').Request
     var TYPES = require('tedious').TYPES;
-
+    // Deployment test
     // This function takes a connection, and emp_id and a callback
     // It creates a request to the connection and returns the rows, then it calls the callback function with the result
     function query_emp_id_data(connection, emp_id, callback) {
